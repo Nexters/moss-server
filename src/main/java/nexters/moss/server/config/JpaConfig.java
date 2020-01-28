38e71484a078b5ee7c6a35a63293ab00c16f2b7d
AgwaB <leesd556@gmail.com>
2020-01-28 20:42:16 +0900

    feat: add postgresql configuration

---
+++ src/main/java/nexters/moss/server/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package nexters.moss.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
