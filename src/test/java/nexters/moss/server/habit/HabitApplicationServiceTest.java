1f7385f4a46b7d309cdfa862af0f43665ff6b972
AgwaB <leesd556@gmail.com>
2020-02-09 23:10:45 +0900

    chore: add junit4

--- src/test/java/nexters/moss/server/habit/HabitApplicationServiceTest.java
+++ src/test/java/nexters/moss/server/habit/HabitApplicationServiceTest.java
@@ -19,8 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
