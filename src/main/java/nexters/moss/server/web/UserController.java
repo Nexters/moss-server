6cebcc8f084a3e23898b575393196118de291494
hihiboss <junhui820@gmail.com>
2020-02-13 02:05:31 +0900

    feat: Implement UserController

--- src/main/java/nexters/moss/server/web/UserController.java
+++ src/main/java/nexters/moss/server/web/UserController.java
@@ -1,9 +1,25 @@
 package nexters.moss.server.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import nexters.moss.server.application.UserApplicationService;
+import nexters.moss.server.application.dto.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+    private UserApplicationService userApplicationService;
+
+    public UserController(UserApplicationService userApplicationService) {
+        this.userApplicationService = userApplicationService;
+    }
+
+    @PostMapping("")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Response<Long> join(
+            @RequestHeader String accessToken,
+            @RequestBody String nickname
+    ) {
+        return userApplicationService.join(accessToken, nickname);
+    }
 }
