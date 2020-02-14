15ac2980ca2d488f1635e7eed4e14aac28c818e9
hihiboss <junhui820@gmail.com>
2020-02-14 21:43:54 +0900

    test: Implement user login test in UserApplicationTest

--- src/test/java/nexters/moss/server/user/UserApplicationServiceTest.java
+++ src/test/java/nexters/moss/server/user/UserApplicationServiceTest.java
@@ -2,9 +2,11 @@ package nexters.moss.server.user;
 
 import nexters.moss.server.application.UserApplicationService;
 import nexters.moss.server.application.dto.Response;
+import nexters.moss.server.application.dto.UserLogin;
 import nexters.moss.server.domain.service.SocialTokenService;
 import nexters.moss.server.domain.model.User;
 import nexters.moss.server.domain.repository.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -19,6 +22,7 @@ import static org.mockito.BDDMockito.given;
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 @SpringBootTest
+@Transactional
 public class UserApplicationServiceTest {
     @Autowired
     private UserRepository userRepository;
@@ -29,25 +33,53 @@ public class UserApplicationServiceTest {
     @Autowired
     private UserApplicationService userApplicationService;
 
+    private String testAccessToken;
+    private User testUser;
+
+    @Before
+    public void setup() {
+        testAccessToken = "accessToken";
+        testUser = User.builder()
+                .socialId(12345678L)
+                .nickname("nickname")
+                .build();
+
+        given(socialTokenService.getSocialUserId(testAccessToken))
+                .willReturn(testUser.getSocialId());
+    }
+
     @Test
     public void joinTest() {
         // given
-        String accessToken = "accessToken";
-        String nickname = "nickname";
-        Long socialId = 12345678L;
-
-        given(socialTokenService.getSocialUserId(accessToken))
-                .willReturn(socialId);
 
         // when
-        Response joinResponse = userApplicationService.join(accessToken, nickname);
+        Response joinResponse = userApplicationService.join(testAccessToken, testUser.getNickname());
 
         // then
         assertThat(joinResponse).isNotNull();
         assertThat(joinResponse.getData()).isNull();
 
-        User user = userRepository.findAll().get(0);
-        assertThat(user.getNickname()).isEqualTo(nickname);
-        assertThat(user.getSocialId()).isEqualTo(socialId);
+        User resultUser = userRepository.findAll().get(0);
+        assertThat(resultUser.getNickname()).isEqualTo(testUser.getNickname());
+        assertThat(resultUser.getSocialId()).isEqualTo(testUser.getSocialId());
+    }
+
+    @Test
+    public void loginTest() {
+        // given
+        userRepository.save(testUser);
+
+        // when
+        Response<UserLogin> loginResponse = userApplicationService.login(testAccessToken);
+
+        // then
+        assertThat(loginResponse).isNotNull();
+        assertThat(loginResponse.getData().getUserId()).isEqualTo(testUser.getId());
+        assertThat(loginResponse.getData().getAccountToken()).isNotNull();
+
+        User resultUser = userRepository.findAll().get(0);
+
+        assertThat(resultUser.getId()).isEqualTo(testUser.getId());
+        assertThat(resultUser.getAccountToken()).isNotNull();
     }
 }
