04d66c0bd6df3e4bed701df2f9d23d79f4d1d4b1
hihiboss <junhui820@gmail.com>
2020-02-19 23:33:26 +0900

    feat: Change HttpServletRequest to Spring Bean

--- src/main/java/nexters/moss/server/web/UserController.java
+++ src/main/java/nexters/moss/server/web/UserController.java
@@ -2,7 +2,6 @@ package nexters.moss.server.web;
 
 import nexters.moss.server.application.UserApplicationService;
 import nexters.moss.server.application.dto.Response;
-import nexters.moss.server.infra.auth.Http;
 import nexters.moss.server.web.value.ReceivedPieceOfCakeRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("user")
 public class UserController {
     private UserApplicationService userApplicationService;
+    private HttpServletRequest httpServletRequest;
 
     public UserController(UserApplicationService userApplicationService) {
         this.userApplicationService = userApplicationService;
@@ -21,29 +21,24 @@ public class UserController {
     @PostMapping("")
     @ResponseStatus(value = HttpStatus.OK)
     public Response join(
-            HttpServletRequest request,
             @RequestBody String nickname
     ) {
         return userApplicationService.join(
-                (String)request.getAttribute("accessToken"),
+                (String) httpServletRequest.getAttribute("accessToken"),
                 nickname
         );
     }
 
     @DeleteMapping("")
     @ResponseStatus(value = HttpStatus.OK)
-    public Response leave(
-            HttpServletRequest request
-    ) {
-        return userApplicationService.leave((Long)request.getAttribute("userId"));
+    public Response leave() {
+        return userApplicationService.leave((Long) httpServletRequest.getAttribute("userId"));
     }
 
     @GetMapping("")
     @ResponseStatus(value = HttpStatus.OK)
-    public Response<String> getUserInfo(
-            HttpServletRequest request
-    ) {
-        return userApplicationService.getUserInfo((Long)request.getAttribute("userId"));
+    public Response<String> getUserInfo() {
+        return userApplicationService.getUserInfo((Long) httpServletRequest.getAttribute("userId"));
     }
 
     @PostMapping("/report")
