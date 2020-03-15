5710acb8677db0e4115623885419f81e0bb58f20
hihiboss <junhui820@gmail.com>
2020-03-15 21:39:45 +0900

    test: Implement successful test cases for join and login in UserController

--- src/test/java/nexters/moss/server/user/TokenControllerTest.java
+++ src/test/java/nexters/moss/server/user/TokenControllerTest.java
@@ -1,14 +1,26 @@
 package nexters.moss.server.user;
 
+import nexters.moss.server.application.UserApplicationService;
+import nexters.moss.server.application.dto.Response;
 import nexters.moss.server.domain.repository.UserRepository;
+import nexters.moss.server.domain.service.SocialTokenService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
@@ -18,15 +30,42 @@ public class TokenControllerTest {
     private TestRestTemplate testRestTemplate;
     @Autowired
     private UserRepository userRepository;
+    @MockBean
+    private SocialTokenService socialTokenService;
+    @Autowired
+    private UserApplicationService userApplicationService;
+
+    private String userAccessToken;
+    private long userSocialId;
+    private String userNickname;
+    private String userHabikeryToken;
 
     @Before
     public void setup() {
+        userAccessToken = "testAccessToken";
+        userSocialId = 12341234L;
+        userNickname = "testNickname";
 
+        when(socialTokenService.getSocialUserId(userAccessToken)).thenReturn(userSocialId);
     }
 
     @Test
-    public void userLoginTest_shouldSuccess() {
+    public void userLoginTest_shouldSuccess() throws URISyntaxException {
+        // given
+        userApplicationService.join(userAccessToken, userNickname);
 
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("accessToken", userAccessToken);
+
+        // when
+        Response<String> response = testRestTemplate.postForObject(
+                new URI("/token"),
+                new HttpEntity<>(null, httpHeaders),
+                Response.class
+        );
+
+        // then
+        assertThat(response.getData()).isNotBlank();
     }
     @Test
     public void userLoginTest_shouldFail_withWrongSocialId() {
--- src/test/java/nexters/moss/server/user/UserControllerTest.java
+++ src/test/java/nexters/moss/server/user/UserControllerTest.java
@@ -1,14 +1,26 @@
 package nexters.moss.server.user;
 
+import nexters.moss.server.application.dto.Response;
+import nexters.moss.server.domain.model.User;
 import nexters.moss.server.domain.repository.UserRepository;
+import nexters.moss.server.domain.service.SocialTokenService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
@@ -18,15 +30,42 @@ public class UserControllerTest {
     private TestRestTemplate testRestTemplate;
     @Autowired
     private UserRepository userRepository;
+    @MockBean
+    private SocialTokenService socialTokenService;
+
+    private String userAccessToken;
+    private long userSocialId;
+    private String userNickname;
+    private String userHabikeryToken;
 
     @Before
     public void setup() {
+        userAccessToken = "testAccessToken";
+        userSocialId = 12341234L;
+        userNickname = "testNickname";
 
+        when(socialTokenService.getSocialUserId(userAccessToken)).thenReturn(userSocialId);
     }
 
     @Test
-    public void userJoinTest_shouldSuccess() {
+    public void userJoinTest_shouldSuccess() throws URISyntaxException {
+        // given
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("accessToken", userAccessToken);
 
+        // when
+        Response response = testRestTemplate.postForObject(
+                new URI("/user"),
+                new HttpEntity<>(userNickname, httpHeaders),
+                Response.class
+        );
+
+        // then
+        assertThat(response).isNotNull();
+
+        User resultUser = userRepository.findBySocialId(userSocialId).get();
+        assertThat(resultUser).isNotNull();
+        assertThat(resultUser.getNickname()).isEqualTo(userNickname);
     }
     @Test
     public void userJoinTest_shouldFail_withWrongSocialId() {
